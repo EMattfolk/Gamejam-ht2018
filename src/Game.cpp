ea6611fae622d681612c100f1710e8dac1d23fe7
Erik Mattfolk <erik.mattfolk@gmail.com>
2018-11-16 20:31:55 +0100

    Updated window title

--- src/Game.cpp
+++ src/Game.cpp
@@ -26,7 +26,7 @@ int main(void)
     const int screenWidth = 800;
     const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "raylib template - simple game");
+    InitWindow(screenWidth, screenHeight, "Swave");
 
     GameScreen currentScreen = LOGO;
 
